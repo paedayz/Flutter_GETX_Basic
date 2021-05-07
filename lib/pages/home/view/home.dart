33209342044586688139574bb618e2cb9e21d1db
paedayz <pae16phasit@gmail.com>
2021-05-07 22:26:25 +0700

    use Obx to use controller state

--- lib/pages/home/view/home.dart
+++ lib/pages/home/view/home.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_1/pages/home/controller/home_controller.dart';
 import 'package:getx_1/pages/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
   final apiService = Get.put(ApiService());
+  final homeController = Get.put(HomeController());
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +18,17 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "This is home screen",
-              style: TextStyle(fontSize: 24),
+            Obx(
+              () => Text(
+                "${homeController.count}",
+                style: TextStyle(fontSize: 24),
+              ),
             ),
             FlatButton(
               onPressed: () {
+                homeController.increment();
                 // print(apiService.fetchTextFromApi());
-                Get.toNamed('/detail');
+                // Get.toNamed('/detail');
                 // Get.to(DetailScreen());
                 // Get.off(DetailScreen());
               },
